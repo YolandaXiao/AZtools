a529d1aecbd8e7b9da040e7d85e866f138dc307a
ankurpapneja <attheencores@gmail.com>
2017-07-18 12:46:59 -0700

    Keep web service only

--- web-service/src/main/java/upload/Application.java
+++ web-service/src/main/java/upload/Application.java
@@ -1,10 +1,7 @@
 package upload;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
