b7a1f3acd1605ea65c13934ec9722700d4832065
ankurpapneja <attheencores@gmail.com>
2017-07-27 16:47:30 -0700

    Implement batch upload and processing

--- AZtools/src/main/java/upload/MainController.java
+++ AZtools/src/main/java/upload/MainController.java
@@ -5,9 +5,7 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import org.jdom.Element;
 import org.jdom.Namespace;
 import org.jdom.output.XMLOutputter;
-import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.XML;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,59 +16,121 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 import pl.edu.icm.cermine.ContentExtractor;
 import pl.edu.icm.cermine.exception.AnalysisException;
 import pl.edu.icm.cermine.tools.timeout.TimeoutException;
 
 import java.io.BufferedInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String listUploadedFiles(Model model) throws IOException {
-        return "uploadForm";
+    public String uploadOnePDF(Model model) throws IOException {
+        return "uploadOnePDF";
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> handleFileUpload(@RequestParam("file") ArrayList<MultipartFile> file,
+    public ResponseEntity<String> processOnePDF(@RequestParam("file") MultipartFile file,
                                                    RedirectAttributes redirectAttributes, Model model)
                                 throws Exception {
 
     	HttpHeaders responseHeaders = new HttpHeaders();
         responseHeaders.add("Content-Type", "application/json; charset=UTF-8");
 
-        ContentExtractor extractor = null;
+        ContentExtractor extractor = new ContentExtractor();
         InputStream inputStream = null;
 
+        ObjectMapper mapper = new ObjectMapper();
+
         try {
-            JSONObject final_result = new JSONObject();
-            ObjectMapper mapper = new ObjectMapper();
+            inputStream = new BufferedInputStream(file.getInputStream());
+            extractor.setPDF(inputStream);
 
-            for (int i = 0; i < file.size(); i++) {
+            String name = file.getOriginalFilename();
+            name = name.split("\\.")[0];
 
-                String name = file.get(i).getOriginalFilename();
+            System.out.println("Applying CERMINE to \"" + name + ".pdf\"...");
+
+            //convert pdf to xml
+            Element nlmMetadata = extractor.getMetadataAsNLM();
+            Element nlmFullText = extractor.getBodyAsNLM(null);
+            Element nlmContent = new Element("article");
+
+            for (Object ns : nlmFullText.getAdditionalNamespaces()) {
+                if (ns instanceof Namespace) {
+                    nlmContent.addNamespaceDeclaration((Namespace) ns);
+                }
+            }
+
+            Element meta = (Element) nlmMetadata.getChild("front").clone();
+            nlmContent.addContent(meta);
+            nlmContent.addContent(nlmFullText);
+
+            System.out.println("! Completed CERMINE workflow.");
+
+            //convert xml to json
+            String nlm = new XMLOutputter().outputString(nlmContent);
+            Attributes attr = new Attributes(nlm, name);
+            //mapper.enable(SerializationFeature.INDENT_OUTPUT);
+
+            String jsonString = mapper.writeValueAsString(attr.toString());
+            String result = jsonString.replace("abstrakt", "abstract");
+
+			return new ResponseEntity<String>(result, responseHeaders, HttpStatus.OK);
+
+		}
+		catch (IOException | TimeoutException | AnalysisException e) {
+			e.printStackTrace();
+			return new ResponseEntity<String>("Exception!!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+    }
+
+    @GetMapping("/batch")
+    public String uploadManyPDFs(Model model) throws IOException {
+        return "uploadManyPDFs";
+    }
+
+    @PostMapping("/batch")
+    public ResponseEntity<String> processManyPDFs(@RequestParam("file") List<MultipartFile> files,
+                                                   RedirectAttributes redirectAttributes, Model model)
+            throws Exception {
+
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.add("Content-Type", "application/json; charset=UTF-8");
+
+        JSONObject final_json = new JSONObject();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+
+            for (int k = 0; k < files.size(); k++) {
+
+                ContentExtractor extractor = new ContentExtractor();
+
+                InputStream inputStream = new BufferedInputStream(((MultipartFile)files.get(k)).getInputStream());
+                extractor.setPDF(inputStream);
+
+                String name = ((MultipartFile)files.get(k)).getOriginalFilename();
                 name = name.split("\\.")[0];
 
-                System.out.println("\nApplying CERMINE to \"" + name + ".pdf\"...");
-
-                extractor = new ContentExtractor();
-                inputStream = new BufferedInputStream(file.get(i).getInputStream());
-                extractor.setPDF(inputStream);
+                System.out.println("Applying CERMINE to \"" + name + ".pdf\"...");
 
                 //convert pdf to xml
                 Element nlmMetadata = extractor.getMetadataAsNLM();
                 Element nlmFullText = extractor.getBodyAsNLM(null);
                 Element nlmContent = new Element("article");
+
                 for (Object ns : nlmFullText.getAdditionalNamespaces()) {
                     if (ns instanceof Namespace) {
                         nlmContent.addNamespaceDeclaration((Namespace) ns);
                     }
                 }
+
                 Element meta = (Element) nlmMetadata.getChild("front").clone();
                 nlmContent.addContent(meta);
                 nlmContent.addContent(nlmFullText);
@@ -79,31 +139,27 @@ public class MainController {
 
                 //convert xml to json
                 String nlm = new XMLOutputter().outputString(nlmContent);
-                JSONObject xmlJSONObj = null;
-                try {
-                    xmlJSONObj = XML.toJSONObject(nlm);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                String pretty_string = xmlJSONObj.toString();
-
-
                 Attributes attr = new Attributes(nlm, name);
                 mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
                 String jsonString = mapper.writeValueAsString(attr);
                 String result = jsonString.replace("abstrakt", "abstract");
 
-                final_result.put(name, result);
+                final_json.put(name, result);
             }
-            String final_json = mapper.writeValueAsString(final_result);
-            return new ResponseEntity<String>(final_json, responseHeaders, HttpStatus.OK);
-		}
-		catch (IOException | TimeoutException | AnalysisException e) {
-			e.printStackTrace();
-			return new ResponseEntity<String>("Exception!!", null, HttpStatus.INTERNAL_SERVER_ERROR);
-		}
+            //mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            String final_json_string = final_json.toString().replace("\\r\\n  \\", "").replace("\\r\\n", "");
+            final_json_string = final_json_string.replace("\\r\\n  ", "").replace("\\\"", "\"");
 
+            //Object json = mapper.readValue(final_json_string, Object.class);
+            //String new_json_string = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+
+            return new ResponseEntity<String>(final_json_string, responseHeaders, HttpStatus.OK);
+        }
+        catch (IOException | TimeoutException | AnalysisException e) {
+            e.printStackTrace();
+            return new ResponseEntity<String>("Exception!!", null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
