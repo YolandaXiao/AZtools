429e5acd842c2b20a0104945842f2a1e3c090399
ankurpapneja <attheencores@gmail.com>
2017-08-17 15:40:29 -0700

    Improve sent clf

--- testing/summary/lib.py
+++ testing/summary/lib.py
@@ -12,15 +12,18 @@ from sklearn import svm
 from textblob.classifiers import NLTKClassifier
 
 ps = PorterStemmer()
-SIZE_WORD_FEATURE_VECTOR = 50
+SIZE_WORD_FEATURE_VECTOR = 250
 AZTOOLS_TIMEOUT = 120
+
+C = 0.1  # for linear svm
+# large value -> smaller margin hyperplane if less misclassifications
+# small value -> larger margin hyperplane even when misclassifications
 
 TRAINING_PDFS_DIRECTORY = "training_pdfs"
 LABELED_SENTENCES_DIR = "training_data/labeled_articles"
 RAW_SENTENCS = "sentences.txt"
 WORD_LIST = "complete_word_list.txt"
 TESTING_DATA = "testing_data.txt"
-
 
 def get_all_pdf():
     print "Grabbing all PDFs from directory"
@@ -29,7 +32,6 @@ def get_all_pdf():
         if file.endswith(".pdf"):
             list_files.append(str(TRAINING_PDFS_DIRECTORY) + "/" + str(file))
     return list_files
-
 
 def get_abstracts(list_files):
     print "Getting all abstracts from AZtools"
@@ -77,7 +79,6 @@ def get_abstracts(list_files):
     print 100.0 * len(abstracts) / len(list_files), "% PDFs processed through AZtools"
     return abstracts
 
-
 def process_abstracts_to_words_csv(abstracts):
     # output: testing_data.txt. each sentence on a separate line, each word separated by comma, words are stemmed
     print "Processing raw abstracts"
@@ -99,11 +100,7 @@ def process_abstracts_to_words_csv(abstracts):
 
             for tok_sent in tokenized_sents:
                 words_wstop = word_tokenize(tok_sent)
-                filtered_words = words_wstop  # [w for w in words_wstop if not w in stop_words]
-                stemmed_words = []
-                for word in filtered_words:
-                    stemmed_words.append(ps.stem(word))
-                final_words.append(stemmed_words)
+                final_words.append(words_wstop)
                 output_line = ""
                 for i in final_words:
                     output_line += i
@@ -113,37 +110,15 @@ def process_abstracts_to_words_csv(abstracts):
             processed_abstracts.append([a[0], final_words])
     return processed_abstracts
 
-
-def process_sentences_to_words_csv():
+def get_sentences():
     sentences = []
-    with open(RAW_SENTENCS, "rb") as sentences_file:
+    with open(TESTING_DATA, "rb") as sentences_file:
         data = sentences_file.readlines()
         for line in data:
             sentences.append(line)
-    ts_data = []
-    with open(TESTING_DATA, "w+") as testing_file:
-        for sent in sentences:
-            tok_words = word_tokenize(sent)
-            line = ""
-            s_sent = []
-            for word in tok_words:
-                try:
-                    s_word = ps.stem(str(word).lower())
-                    s_sent.append(s_word)
-                    line += s_word
-                    line += ","
-                except Exception as ex:
-                    continue
-            if line is not None:
-                line = line[:-1]
-                testing_file.write(line + "\n")
-                ts_data.append([s_sent, sent])
-            else:
-                continue
-    return ts_data
+    return sentences
 
-
-def get_popular_words(processed_abstracts):
+def get_popular_words_from_abstracts(processed_abstracts):
     print "Getting all possible words"
     all_words = dict()
     for a in processed_abstracts:
@@ -164,16 +139,42 @@ def get_popular_words(processed_abstracts):
             break
     return pop_words
 
+def get_popular_words_from_sentences(sentences):
+    print "Getting all possible words"
+    all_words = dict()    
+    stop_words = []
+    nltk_stopwords = set(stopwords.words('english'))
+    common_words = ['.', ',', ':', ')', '('] 
+    with open("stopwords.txt", "rb") as stopwords_file:
+        data = stopwords_file.readlines()
+        for line in data:
+            stop_words.append(line.strip('\n'))
+        for s in sentences:
+            tok_words = word_tokenize(s)
+            for word in tok_words:
+                if word in stop_words or word in common_words:
+                    continue
+                if all_words.has_key(word):
+                    all_words[word] += 1
+                else:
+                    all_words[word] = 1
+
+    print "Finding", SIZE_WORD_FEATURE_VECTOR, "most popular words"
+    sorted_words = sorted(all_words.items(), key=operator.itemgetter(1))
+    pop_words = []
+    for word in reversed(sorted_words):
+        pop_words.append(word[0])
+        if len(pop_words) >= SIZE_WORD_FEATURE_VECTOR:
+            break
+    return pop_words
 
 def get_word_list():
     output_words = []
     with open(WORD_LIST, "rb") as input_file:
         input_words = input_file.readlines()
         for word in input_words:
-            output_words.append(word.lower().strip("\r\n"))
-    unique_words = list(set(output_words))
-    return unique_words
-
+            output_words.append(word.strip("\r\n"))
+    return output_words
 
 def create_feature_vector_from_raw_sent(sentence, features):
     # second paramter can be get_popular_words() or get_word_list()
@@ -187,7 +188,6 @@ def create_feature_vector_from_raw_sent(sentence, features):
             f_vector[index] = 1
     return f_vector
 
-
 def create_feature_vector_from_tok_sent(sentence, features):  
     # first parameter is a list of tokenized and stemmed words
     # list of sents can be obtained from process_sentences_to_words_csv()
@@ -200,7 +200,6 @@ def create_feature_vector_from_tok_sent(sentence, features):
             f_vector[index] = 1
     return f_vector
 
-
 def get_training_txt_files():
     list_files = []
     for file in os.listdir(LABELED_SENTENCES_DIR):
@@ -208,59 +207,62 @@ def get_training_txt_files():
             list_files.append(str(LABELED_SENTENCES_DIR) + "/" + str(file))
     return list_files
 
-
 def combine_training_sentences(list_files):
     with open("training_data.txt", "w+") as output_file:
         for file in list_files:
             with open(file, "rb") as input_file:
                 data = input_file.readlines()
                 for line in data:
+                    new_line = None
                     if line[0] == "#":
                         continue
-                    info = line.split("\t")
+                    elif line[4] == " ":
+                        new_line = line[:4] + "\t" + line[5:]
+                    elif line[4:6] == "--":
+                        new_line = line[:4] + "\t" + line[6:]
+                    else:
+                        new_line = line
+                    info = new_line.split("\t")
                     if len(info) <= 1:
+                        print "Less than = to size 1"
                         continue
                     class_id = 0
                     class_val = info[0].strip(" ")
                     if class_val == "AIMX" or class_val == "OWNX":
                         class_id = 0
-                    elif class_val == "BASE" or class_val == "CONT":
+                    elif class_val == "BASE" or class_val == "CONT" or class_val == "MISC":
                         class_id = 1
-                    elif class_val == "MISC":
-                        class_id = 2
                     else:
                         print "Could not identify:", class_val
                         continue
-                    tok_sent = word_tokenize(info[1])
-                    ts_words = []
-                    output_line = str(class_id) + ","
-                    for word in tok_sent:
-                        s_word = str(ps.stem(word.lower()))
-                        ts_words.append(s_word)
-                        output_line += s_word
-                        output_line += ","
-                    output_file.write(output_line[:-1] + "\n")
+                    line_to_write = str(class_id) + "\t" + info[1]
+                    output_file.write(line_to_write)
 
-def get_all_labeled_sentences():
-    training_sentences = []
-    testing_sentences = []
-    with open('training_data.txt', "rb") as input_file:
+def clean_complete_dataset():
+    with open("complete_training_dataset.txt", "rb") as input_file, open("final_dataset.txt", "w+") as output_file:
         data = input_file.readlines()
         for line in data:
-            info = line.split(",")
-            class_id = info[0]
-            words = info[1:-1]
-            if random.random() >= 0.9:
-                testing_sentences.append([class_id, words])
+            if line[1] == " ":
+                if line[0] == 2:
+                    output_file.write(str(1) + "\t" + line[2:])
+                else:
+                    output_file.write(line[0] + "\t" + line[2:])    
+            elif line[1:3] == "--":
+                if line[0] == 2:
+                    output_file.write(str(1) + "\t" + line[3:])
+                else:
+                    output_file.write(line[0] + "\t" + line[3:])
             else:
-                training_sentences.append([class_id, words])
-    return training_sentences, testing_sentences
+                if line[0] == 2:
+                    output_file.write(str(1) + line[1:])
+                else:
+                    output_file.write(line)
 
-
-def get_all_labeled_sentences_not_tok():
+def get_all_labeled_sentences(percent_training, file_name):
     training_sentences = []
     testing_sentences = []
-    with open('final_dataset.txt', "rb") as input_file:
+    total = []
+    with open(file_name, "rb") as input_file:
         data = input_file.readlines()
         for line in data:
             info = line.split("\t")
@@ -269,50 +271,50 @@ def get_all_labeled_sentences_not_tok():
                 print info
                 continue
             sentence = info[1]
-            if random.random() >= 0.9:
-                testing_sentences.append([class_id, sentence])
-            else:
-                training_sentences.append([class_id, sentence])
+            total.append([class_id, sentence])
+    random.shuffle(total)
+    thres = int(len(total) * percent_training)
+    training_sentences = total[:thres]
+    testing_sentences = total[thres:]
     return training_sentences, testing_sentences
 
-
 def svm_train(x, y):
-    lin_clf = svm.LinearSVC(C=.01)
+    lin_clf = svm.LinearSVC(C=C)
     lin_clf.fit(x, y)
     return lin_clf
-
 
 def svm_predict(lin_clf, test_sample):
     return lin_clf.predict([test_sample])
 
+def svm_get_accuracy(lin_clf, X, y):
+    return lin_clf.score(X, y)
 
-def clean_complete_dataset():
-    with open("complete_training_dataset.txt", "rb+") as input_file, open("final_dataset.txt", "w+") as output_file:
-        data = input_file.readlines()
-        for line in data:
-            if line[1] == " ":
-                output_file.write(line[0] + "\t" + line[2:])
-            elif line[1:3] == "--":
-                output_file.write(line[0] + "\t" + line[3:])
-            else:
-                output_file.write(line)
+def train_and_test_on_main():
 
+    # # Option 1: (training data 1)
+    # print "Cleaning dataset"
+    # clean_complete_dataset()
 
-if __name__ == '__main__':
+    # Option 2: (training data 2)
+    print "Getting training data"
+    files = get_training_txt_files()
+    combine_training_sentences(files)
 
-    clean_complete_dataset()
+    #---------- May use option one or two
 
     # get features
-    word_list = get_word_list()
+    print "Getting word list"
+    word_list = get_popular_words_from_sentences(get_sentences())#get_word_list()
 
+    total_num_sents = 0
     average_accuracy = 0
     for i in range(100):
-        
         # structure data
-        training_labeled_data, testing_labeled_data = get_all_labeled_sentences_not_tok()
-        # print "num training", len(training_labeled_data)
-        # print "total", len(training_labeled_data) + len(testing_labeled_data)
 
+        # if want to use 1st dataset for training (the same), use file name 'final_dataset.txt' and uncomment option 1
+        training_labeled_data, testing_labeled_data = get_all_labeled_sentences(.9, 'training_data.txt')
+        total_num_sents = len(training_labeled_data) + len(testing_labeled_data)
+        
         # train model
         X = []
         y = []
@@ -320,32 +322,64 @@ if __name__ == '__main__':
             X.append(create_feature_vector_from_raw_sent(case[1], word_list))
             y.append(case[0])
         model = svm_train(X, y)
-
+        
         # test model
-        num_correct = 0
-        total = 0
+        X = []
+        y = []
         for case in testing_labeled_data:
-            X = (create_feature_vector_from_raw_sent(case[1], word_list))
-            y = case[0]
-            prediction = svm_predict(model, X)[0]
-            if y == prediction:
-                num_correct += 1
-            total += 1
-        accuracy = num_correct * 100.0 / total
-        print "Accuracy: " + str(accuracy) + "%"
-        average_accuracy += accuracy
-
+            X.append(create_feature_vector_from_raw_sent(case[1], word_list))
+            y.append(case[0])
+        acc = svm_get_accuracy(model, X, y) * 100
+        print "Accuracy " + str(i+1) + str(": %.2f" % round(acc,2)) + "%"
+        average_accuracy += acc
     print "--------------------------"
     average_accuracy /= 100
-    print "Average Accuracy: " + str(average_accuracy) + "%"
+    print "Average Accuracy: " + str("%.2f" % round(average_accuracy,2)) + "%"
+    print "C = " + str(C)
+    print "Total sentences: " + str(total_num_sents) + "\n90% trained, 10% tested"
 
-    # # apply model to abstracts
-    # sents = process_sentences_to_words_csv()
-    # features = []
-    # for sent in sents:
-    #     fv = create_feature_vector_from_tok_sent(sent[0], word_list)
-    #     features.append([fv, sent[0]])
-    # with open("output.txt", "w+") as output_file:
-    #     for i in range(len(features)):
-    #         output_line = str(svm_predict(model, features[i][0])[0]) + "\t" + str(sents[i][1]) + "\n"
-    #         output_file.write(output_line)
+def apply_model_to_sentences():
+    # # Option 1:
+    # print "Cleaning dataset"
+    # clean_complete_dataset()
+
+    # Option 2:
+    print "Getting training data"
+    files = get_training_txt_files()
+    combine_training_sentences(files)
+
+    #---------- May use option one or two
+    # get features
+    print "Getting word list"
+    word_list = get_popular_words_from_sentences(get_sentences())#get_word_list()
+
+    # train model
+    print "Training model"
+    X = []
+    y = []
+
+    # if want to use 1st dataset for training (the same), use file name 'final_dataset.txt' and uncomment option 1
+    training_labeled_data, testing_labeled_data = get_all_labeled_sentences(1, 'training_data.txt')
+    for case in training_labeled_data:
+        X.append(create_feature_vector_from_raw_sent(case[1], word_list))
+        y.append(case[0])
+    model = svm_train(X, y)
+
+    # apply model on abstracts' sentences
+    print "Applying model to sentences from abstracts"
+    sents = get_sentences()
+    features = []
+    for sent in sents:
+        fv = create_feature_vector_from_raw_sent(sent, word_list)
+        features.append([fv, sent])
+    with open("output.txt", "w+") as output_file:
+        for i in range(len(features)):
+            output_line = str(svm_predict(model, features[i][0])[0]) + "\t" + str(sents[i]) + "\n"
+            output_file.write(output_line)
+
+    print "See output.txt for results"
+
+if __name__ == '__main__':
+    apply_model_to_sentences()
+    train_and_test_on_main()
+    
