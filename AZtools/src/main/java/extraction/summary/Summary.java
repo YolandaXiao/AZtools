d8f25e7bada82a9cd729c0ec5a7cd00ab9f3f465
ankurpapneja <attheencores@gmail.com>
2017-08-22 16:13:48 -0700

    Time summary methods

--- AZtools/src/main/java/extraction/summary/Summary.java
+++ AZtools/src/main/java/extraction/summary/Summary.java
@@ -3,8 +3,9 @@ package extraction.summary;
 import extraction.Properties;
 import extraction.SystemCommandExecutor;
 
-//import java.io.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class Summary {
@@ -14,53 +15,94 @@ public class Summary {
     public Summary(String paper_abstract, String filename, String tool_name) {
 
         try {
-//            String abs_summ_dir = Properties.get_abs_summ_dir();
-//            String wr_txt_file_name = abs_summ_dir + filename + "_abstract.txt";
-//            String rd_txt_file_name = abs_summ_dir + filename + "_summary.txt";
-//
-//            File file = new File(wr_txt_file_name);
-//            FileWriter writer = new FileWriter(file);
-//            writer.write(paper_abstract);
-//            writer.close();
+            Calendar start = Calendar.getInstance();
+            String abs_summ_dir = Properties.get_abs_summ_dir();
+            String wr_txt_file_name = abs_summ_dir + filename + "_abstract.txt";
+            String rd_txt_file_name = abs_summ_dir + filename + "_summary.txt";
+            Calendar mid1 = Calendar.getInstance();
+
+            File file = new File(wr_txt_file_name);
+            FileWriter writer = new FileWriter(file);
+            writer.write(paper_abstract);
+            writer.close();
+            Calendar mid2 = Calendar.getInstance();
 
             List<String> commands = new ArrayList<>();
             commands.add("python");
             String svm_lib_path = Properties.get_svm_lib_path();
             commands.add(svm_lib_path);
-            commands.add(paper_abstract);
-//            commands.add(filename);
+//            commands.add(paper_abstract);
+            commands.add(filename);
             commands.add(tool_name);
+            Calendar mid3 = Calendar.getInstance();
 
-            SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commands);
-            int result = commandExecutor.executeCommand();
-            StringBuilder stdout = commandExecutor.getStandardOutputFromCommand();
-
-            if (result != 0) {
-                StringBuilder stderr = commandExecutor.getStandardErrorFromCommand();
-
-                System.out.println("Exit status: " + result);
-                System.out.println("STDOUT:");
-                System.out.println(stdout);
-                System.out.println("STDERR:");
-                System.out.println(stderr);
-            } else {
-                summary = stdout.toString();
+            Calendar mid4 = Calendar.getInstance();
+            ProcessBuilder pb = new ProcessBuilder(commands);
+            Process process = pb.start();
+            int rv = process.waitFor();
+            Calendar mid5 = Calendar.getInstance();
+            if (rv != 0) {
+                InputStream errorStream = process.getErrorStream();
+                System.out.println("Exit value: " + rv);
+                System.out.println("error stream: " + errorStream);
+                summary = "Error while finding summary";
             }
 
-//            FileReader fr = new FileReader(rd_txt_file_name);
-//            BufferedReader br = new BufferedReader(fr);
-//            String line;
-//            if ((line = br.readLine()) != null) {
-//                summary = line;
+//            SystemCommandExecutor commandExecutor = new SystemCommandExecutor(commands);
+//            Calendar mid4 = Calendar.getInstance();
+//            int result = commandExecutor.executeCommand();
+//            Calendar mid5 = Calendar.getInstance();
+
+//            StringBuilder stderr = commandExecutor.getStandardErrorFromCommand();
+//            StringBuilder stdout = commandExecutor.getStandardOutputFromCommand();
+
+//            if (result != 0) {
+//                System.out.println("Exit status: " + result);
+//                System.out.println("STDOUT:");
+//                System.out.println(stdout);
+//                System.out.println("STDERR:");
+//                System.out.println(stderr);
+//            } else {
+//                System.out.println("Exit status: " + result);
+//                System.out.println("STDOUT:");
+//                System.out.println(stdout);
+//                System.out.println("STDERR:");
+//                System.out.println(stderr);
+//                summary = stdout.toString();
 //            }
-//            else {
-//                summary = paper_abstract;
-//            }
+
+            Calendar mid6 = Calendar.getInstance();
+
+            FileReader fr = new FileReader(rd_txt_file_name);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            if ((line = br.readLine()) != null) {
+                summary = line;
+            }
+            else {
+                summary = paper_abstract;
+            }
+//            Calendar mid7 = Calendar.getInstance();
+//            System.out.println("start to mid1: ");
+//            System.out.println(mid1.getTimeInMillis()-start.getTimeInMillis());
+//            System.out.println("mid1 to mid2: ");
+//            System.out.println(mid2.getTimeInMillis()-mid1.getTimeInMillis());
+//            System.out.println("mid2 to mid3: ");
+//            System.out.println(mid3.getTimeInMillis()-mid2.getTimeInMillis());
+            System.out.println("mid3 to mid4: ");
+            System.out.println(mid4.getTimeInMillis()-mid3.getTimeInMillis());
+//            System.out.println("mid4 to mid5: ");
+//            System.out.println(mid5.getTimeInMillis()-mid4.getTimeInMillis());
+//            System.out.println("mid5 to mid6: ");
+//            System.out.println(mid6.getTimeInMillis()-mid5.getTimeInMillis());
+//            System.out.println("mid6 to mid7: ");
+//            System.out.println(mid7.getTimeInMillis()-mid6.getTimeInMillis());
         }
         catch (Exception e){
             System.out.println("Failed to get summary!");
             e.printStackTrace();
         }
+
     }
 
     public String getSummary() {
--- AZtools/src/main/java/extraction/summary/abstract_to_summary.py
+++ AZtools/src/main/java/extraction/summary/abstract_to_summary.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 
 from nltk.tokenize import sent_tokenize, word_tokenize
 from sklearn import svm
@@ -43,39 +44,60 @@ def svm_predict(lin_clf, test_sample):
     return lin_clf.predict([test_sample])
 
 
-# if len(sys.argv) != 3:
-#     print "Incorrect call to script.\nUsage: 'python lib.py file_name.pdf tool_name'"
-#     sys.exit(1)
+start = time.time()
 
 if len(sys.argv) != 3:
-    print "Incorrect call to script.\nUsage: 'python lib.py paper_abstract tool_name'"
+    print "Incorrect call to script.\nUsage: 'python abstract_to_summary.py file_name.pdf tool_name'"
     sys.exit(1)
+
+# if len(sys.argv) != 3:
+#     print "Incorrect call to script.\nUsage: 'python abstract_to_summary.py paper_abstract tool_name'"
+#     sys.exit(1)
 
 word_list = get_word_list()
 model = joblib.load(MODEL_PKL)
 summary = ""
 
-# filename = sys.argv[1]
+filename = sys.argv[1]
 tool_name = sys.argv[2]
 
-# input_filename = directory + "/abs_to_summ/" + filename + "_abstract.txt"
-# output_filename = directory + "/abs_to_summ/" + filename + "_summary.txt"
+input_filename = directory + "/abs_to_summ/" + filename + "_abstract.txt"
+output_filename = directory + "/abs_to_summ/" + filename + "_summary.txt"
 
+mid1 = time.time()
+print "start to mid1", mid1-start
 
-# with open(input_filename, "rb") as input_file, open(output_filename, "w+") as output_filename:
-#     paragraph = input_file.readlines()[0]
-#     sentences = sent_tokenize(paragraph)
-#     for sent in sentences:
-#         fv = create_feature_vector_from_raw_sent(sent, word_list)
-#         if int(svm_predict(model, fv)[0]) == 0 or tool_name.lower() in sent.lower():
-#             summary = summary + sent + " "
-#     summary = summary[:-1]
-#     output_filename.write(summary)
+with open(input_filename, "rb") as input_file, open(output_filename, "w+") as output_filename:
+    mid2 = time.time()
+    paragraph = input_file.readlines()[0]
+    mid25 = time.time()
+    sentences = sent_tokenize(paragraph)
+    mid3 = time.time()
+    print ": mid2 to 25", mid25-mid2    
+    print ": mid2 to 3", mid3-mid2
+    for sent in sentences:
+        mid4 = time.time()
+        fv = create_feature_vector_from_raw_sent(sent, word_list)
+        mid5 = time.time()
+        print ":: mid4 to 5", mid5-mid4
+        if int(svm_predict(model, fv)[0]) == 0 or tool_name.lower() in sent.lower():
+            summary = summary + sent + " "
+        mid6 = time.time()
+        print ":: mid5 to 6", mid6-mid5
+    mid7 = time.time()
+    print ": mid3 to 7", mid7-mid3
+    summary = summary[:-1]
+    output_filename.write(summary)
+    mid8 = time.time()
+    print ": mid7 to 8", mid8-mid7
 
-sentences = sent_tokenize(sys.argv[1])
-for sent in sentences:
-    fv = create_feature_vector_from_raw_sent(sent, word_list)
-    if int(svm_predict(model, fv)[0]) == 0 or tool_name.lower() in sent.lower():
-        summary = summary + sent + " "
-summary = summary[:-1]
-print summary
+end = time.time()
+print "mid1 to end", end-mid1
+print "start to end", end-start
+# sentences = sent_tokenize(sys.argv[1])
+# for sent in sentences:
+#     fv = create_feature_vector_from_raw_sent(sent, word_list)
+#     if int(svm_predict(model, fv)[0]) == 0 or tool_name.lower() in sent.lower():
+#         summary = summary + sent + " "
+# summary = summary[:-1]
+# print summary
