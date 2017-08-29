b015ad6c97740ccbaa627ff1c903abbccb5e5f3a
ankurpapneja <attheencores@gmail.com>
2017-08-28 18:08:46 -0700

    Properties to Paths

---
+++ AZtools/src/main/java/extraction/.Paths.java
@@ -0,0 +1,42 @@
+//package extraction;
+//
+//public class Properties {
+//
+//    private static String basic_java = "AZtools/src/main/java/extraction/";
+//    private static String agency_path = "AZtools/lib/agency_names.txt";
+//    private static String serializedClassifier = "AZtools/lib/stanford-ner-2017-06-09/classifiers/english.conll.4class.distsim.crf.ser.gz";
+//
+//    private static String en_path = basic_java + "name/en.txt";
+//    private static String stop_path = basic_java + "name/stop.txt";
+//    private static String mesh_path = basic_java + "name/mesh_terms.txt";
+//    private static String abs_summ_dir = basic_java + "summary/abs_to_summ/";
+//    private static String svm_lib_path = basic_java + "summary/abstract_to_summary.py";
+//
+//    public static String get_en_path() {
+//        return en_path;
+//    }
+//
+//    public static String get_stop_path() {
+//        return stop_path;
+//    }
+//
+//    public static String get_mesh_path() {
+//        return mesh_path;
+//    }
+//
+//    public static String get_abs_summ_dir() {
+//        return abs_summ_dir;
+//    }
+//
+//    public static String get_svm_lib_path() {
+//        return svm_lib_path;
+//    }
+//
+//    public static String getAgencyNamesFileName() {
+//        return agency_path;
+//    }
+//
+//    public static String getSerializedClassifier() {
+//        return serializedClassifier;
+//    }
+//}
