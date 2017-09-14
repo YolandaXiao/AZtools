1ffe48d3dca21bb1199d851aa9a1c2cfce1efa1d
ankurpapneja <attheencores@gmail.com>
2017-09-14 16:29:10 -0700

    New command

--- command.py
+++ command.py
@@ -10,7 +10,7 @@ d = c.replace("C:\Ankur\Code\AZtools", "/home/developer/AZtools")
 e = d.replace("C:\Users\\ankur\.m2\\repository", "/home/developer/.m2/repository")
 f = e.replace("\\", "/")
 
-with open("out.txt", "w+") as file:
+with open("linux_comm.txt", "w+") as file:
 	intro = '/usr/bin/java "-javaagent:/home/developer/.local/share/umake/ide/idea/lib/idea_rt.jar=63619:/home/developer/.local/share/umake/ide/idea/bin" -Dfile.encoding=UTF-8 '
 	file.write(intro)
 	file.write(f)
