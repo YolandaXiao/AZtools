2b499a2d19ce9b077e43ebf6f1529cbca47d0d90
ankurpapneja <attheencores@gmail.com>
2017-09-14 17:07:54 -0700

    New library

--- command.py
+++ command.py
@@ -1,5 +1,5 @@
 
-with open("command.txt", "rb") as file:
+with open("command_input.txt", "rb") as file:
 	data = file.readlines()
 	for line in data:
 		a = line
@@ -10,7 +10,7 @@ d = c.replace("C:\Ankur\Code\AZtools", "/home/developer/AZtools")
 e = d.replace("C:\Users\\ankur\.m2\\repository", "/home/developer/.m2/repository")
 f = e.replace("\\", "/")
 
-with open("linux_comm.txt", "w+") as file:
+with open("command_output.txt", "w+") as file:
 	intro = '/usr/bin/java "-javaagent:/home/developer/.local/share/umake/ide/idea/lib/idea_rt.jar=63619:/home/developer/.local/share/umake/ide/idea/bin" -Dfile.encoding=UTF-8 '
 	file.write(intro)
 	file.write(f)
